1175dab246fcf176a27b75e699b976981d02f7b1
unknown <sddavid.johan@gmail.com>
2021-02-28 16:02:38 +0530

    README: Added link

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Portfolio
+View live site [here](https://davidsling.in)
--- src/App.js
+++ src/App.js
@@ -16,12 +16,15 @@ export default function App() {
     <div className="App">
     <Router>
       <AppBar pageNow={pageNow} setPageNow={setPageNow} navOpen={navOpen} setNavOpen={setNavOpen} />
-      <Route exact path='/'>
-        <Home setPageNow={setPageNow} />
-      </Route>
-      <Route exact path='/contact'>
-        <Contact setPageNow={setPageNow} />
-      </Route>
+      <Switch>
+        <Route exact path='/'>
+          <Home setPageNow={setPageNow} />
+        </Route>
+        <Route exact path='/contact'>
+          <Contact setPageNow={setPageNow} />
+        </Route>
+        <Route path='/'><h1>404</h1></Route>
+      </Switch>
     </Router></div>
   )
 }
