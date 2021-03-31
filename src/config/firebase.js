add7f05a574ccfcc68b7383539897d95ce484b29
unknown <sddavid.johan@gmail.com>
2021-03-31 21:36:12 +0530

    Added contact form

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,16 +1,24 @@
-import env from "react-dotenv";
-var processEnv = {};
-if (process.env.NODE_ENV == "development")
-  processEnv = {
-    apiKey: "AIzaSyDfvFYu5wVxOltOIiTkzMyHu_B2NnnF5kA",
-    authDomain: "davidsling-in.firebaseapp.com",
-    projectId: "davidsling-in",
-    storageBucket: "davidsling-in.appspot.com",
-    messagingSenderId: "133782068887",
-    appId: "1:133782068887:web:bd97c101d52fa2127dd3d2",
-    measurementId: "G-0PEE2GHPGM",
-  };
-else processEnv = process.env;
+var processEnv = {
+  apiKey: "AIzaSyDfvFYu5wVxOltOIiTkzMyHu_B2NnnF5kA",
+  authDomain: "davidsling-in.firebaseapp.com",
+  projectId: "davidsling-in",
+  storageBucket: "davidsling-in.appspot.com",
+  messagingSenderId: "133782068887",
+  appId: "1:133782068887:web:bd97c101d52fa2127dd3d2",
+  measurementId: "G-0PEE2GHPGM",
+};
+
+// if (process.env.NODE_ENV == "development")
+//   processEnv = {
+//     apiKey: "AIzaSyDfvFYu5wVxOltOIiTkzMyHu_B2NnnF5kA",
+//     authDomain: "davidsling-in.firebaseapp.com",
+//     projectId: "davidsling-in",
+//     storageBucket: "davidsling-in.appspot.com",
+//     messagingSenderId: "133782068887",
+//     appId: "1:133782068887:web:bd97c101d52fa2127dd3d2",
+//     measurementId: "G-0PEE2GHPGM",
+//   };
+// else processEnv = process.env;
 
 const {
   apiKey,
