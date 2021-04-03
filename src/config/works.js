aaa2bcc94d2ce92f01a684adcb968e9ac7a5a3ed
DAVID <sddavid.johan@gmail.com>
2021-04-03 12:41:12 +0530

    Changed work images | Added Abrahams associates

--- src/config/works.js
+++ src/config/works.js
@@ -1,47 +1,46 @@
-import starpolaris from "../assets/starpolaris.svg";
 import pebble from "../assets/pebble.svg";
 import flamebond from "../assets/flamebond.svg";
-import davidslingadmin from "../assets/davidslingadmin.png";
+import davidslingadmin from "../assets/davidslingadmin.svg";
 import dreamcap from "../assets/dreamcap.svg";
+import abrahams from "../assets/abrahams.svg";
 
 const works = [
   {
     image: pebble,
-    subHead: "MERN + React-Native + Socket.io + Design",
+    subHead: "LOGO Design + MERN + React-Native + Socket.io",
     title: "Pebble",
-    description: "Pebble is a chat application built for the Web and android",
+    description: "Pebble is a chat application built for the Web and android.",
     link: "https://pebble.davidsling.in",
   },
   {
     image: dreamcap,
     subHead: "HTML + CSS + Design",
     title: "DREAMCAP",
-    description: "A fun project made for my friends using only HTML and CSS",
+    description: "A fun project made for my friends using only HTML and CSS.",
     link: "https://dreamcap.netlify.app/",
   },
   {
     image: flamebond,
-    subHead: "Frontend Development (React JS) + Firebase + Design",
+    subHead: "LOGO Design + React JS + Firebase",
     title: "Flame Bond",
     description:
-      "A reusable content management system that connects React js with firebase",
+      "A reusable content management system that connects React js with firebase.",
     link: "https://github.com/david-sling/flame-bond",
   },
   {
     image: davidslingadmin,
-    subHead: "Built using Flame Bond",
+    subHead: "LOGO Design + Develpment",
     title: "Admin panel for davidsling.in",
     description:
-      "An admin panel to view messages sent from davidsling.in/contact. This app is built using the above project, 'Flame Bond'. You can see the login page by clicking here, but you cannot login",
+      "An admin panel to view messages sent from davidsling.in/contact. This app is built using the above project, 'Flame Bond'. You can see the login page by clicking here, but you cannot login.",
     link: "https://admin.davidsling.in",
   },
   {
-    image: starpolaris,
-    subHead: "Frontend Development (React JS) + Design",
-    title: "Starpolaris LLP",
-    description:
-      "Starpolaris is a film distribution company. The created website has four pages and a unique navigation animation",
-    link: "http://starpolarisllp.netlify.app",
+    image: abrahams,
+    subHead: "LOGO Design",
+    title: "Abraham's Associates",
+    description: "Abrahams's associates is a trading and service company.",
+    link: "https://www.instagram.com/abrahamsasociates/",
   },
 ];
 
