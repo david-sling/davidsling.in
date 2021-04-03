a02952222bcb50bdd8d3b28216203ab7104664a9
DAVID <sddavid.johan@gmail.com>
2021-04-03 18:23:55 +0530

    Lazy load images

--- src/components/Home/WorkCard.jsx
+++ src/components/Home/WorkCard.jsx
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { Link } from "react-router-dom";
+import ReactVisibilitySensor from "react-visibility-sensor";
 
 export default function WorkCard({ work }) {
+  const [imageLoaded, setImageLoaded] = useState(false);
   return (
     <a href={work.link} target="_blank">
       <div className="WorkCard">
-        <div className="image">
-          <img src={work.image} alt={work.title + "-davidsling"} />
-        </div>
+        <ReactVisibilitySensor
+          onChange={(isVisible) => {
+            if (isVisible) {
+              setImageLoaded(true);
+              // console.log(result.name)
+            }
+          }}
+        >
+          <div className="image">
+            {imageLoaded && (
+              <img src={work.image} alt={work.title + "-davidsling"} />
+            )}
+          </div>
+        </ReactVisibilitySensor>
         <div className="content">
           <h5 className="subtitle"> {work.subHead} </h5>
           <h4 className="title">{work.title}</h4>
