c4106c8036b0be1a3183084fccac478f9c49530d
DAVID <sddavid.johan@gmail.com>
2021-04-03 15:59:40 +0530

    Implemented React/lazy()

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, lazy, Suspense } from "react";
 import "./css/App.css";
 
 //IMPORT LIBRARIES
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 //IMPORT ROUTES
 import AppBar from "./components/AppBar";
-import Home from "./components/Home/Home";
-import Contact from "./components/Contact/Contact";
+import Loading from "./components/Home/Loading";
+// import Home from "./components/Home/Home";
+// import Contact from "./components/Contact/Contact";
+
+const Home = lazy(() => import("./components/Home/Home"));
+const Contact = lazy(() => import("./components/Contact/Contact"));
 
 export default function App() {
   const [pageNow, setPageNow] = useState(null);
@@ -23,10 +27,14 @@ export default function App() {
         />
         <Switch>
           <Route exact path="/">
-            <Home setPageNow={setPageNow} />
+            <Suspense fallback={<Loading />}>
+              <Home setPageNow={setPageNow} />
+            </Suspense>
           </Route>
           <Route exact path="/contact">
-            <Contact setPageNow={setPageNow} />
+            <Suspense fallback={<Loading />}>
+              <Contact setPageNow={setPageNow} />
+            </Suspense>
           </Route>
           <Route path="/">
             <h1>404</h1>
